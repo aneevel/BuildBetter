e33e63d288e06ebc38523b624b456b1e4fee5410
Alec Neevel <aneevel15@gmail.com>
2022-06-28 16:05:08 -0500

    Add routing and navigation links

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutComponent,
+    HomeComponent,
+    TasksComponent,
+    UserSettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'tasks/:userId', component: TasksComponent },
+      { path: 'settings/:userId', component: UserSettingsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
